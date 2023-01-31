3da324365ba6e6b99097e99c0519db2502a06ac2
Mario Villalba <villalbamario18@gmail.com>
2023-01-31 15:32:13 -0300

    modificacion

--- api/src/app.js
+++ api/src/app.js
@@ -20,14 +20,14 @@ server.use(
     limits: { fileSize: 50 * 1024 * 1024 },
   })
 );
-server.use(cors({ origin: "https://gamescript-app.vercel.app" }));
+//server.use(cors({ origin: "https://gamescript-app.vercel.app/" }));
 //aca termina jajas
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", 'https://gamescript-app.vercel.app/'); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
